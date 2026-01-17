ff368a4c30195d2d82b7a92116b8d1a45807d503
Eric Lin <ericlinyi@gmail.com>
2026-01-17 22:48:19 +0800

    add log

--- index.tsx
+++ index.tsx
@@ -3,6 +3,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+console.log("VITE_API_KEY=", import.meta.env.VITE_API_KEY);
+console.log("MODE=", import.meta.env.MODE);
+console.log("PROD=", import.meta.env.PROD);
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
